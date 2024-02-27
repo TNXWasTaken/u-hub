8d1c3b2b99529157d2ef7acb1e48a3a7fe4923f4
TNXWasTaken <118904290+TNXWasTaken@users.noreply.github.com>
2024-02-27 11:19:58 +0000

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.addEventListener("keydown", function (e) {
 	if (e.key == "q" && e.ctrlKey) {
 		var t = window.open("", "_blank", "width=500,height=300");
 		var e = t.document.createElement("iframe");
-		(e.src = ""),
+		(e.src = "https://raw.githubusercontent.com/TNXWasTaken/u-hub/main/main.html"),
 			(e.style.cssText = "width:100%; height:100%; border:none;"),
 			t.document.body.appendChild(e),
 			t.document.title = "uRun",
